e3f2a4e3fbe333f696c8c39bb93fef7528fc0ae4
arwildo <arwildo@gmail.com>
2019-10-30 12:15:08 +0800

    add srollUp function

--- main.js
+++ main.js
@@ -30,6 +30,23 @@ function clickOutSide(e) {
 	}
 }
 
+// Scroll To Top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        document.getElementById("scrollToTop").style.display = "block";
+    } else {
+        document.getElementById("scrollToTop").style.display = "none";
+    }
+   
+}
+
+function topFunction() {
+ 
+     $('html, body').animate({scrollTop:0}, 'slow');
+}
+
 // Google Analytic
 window.dataLayer = window.dataLayer || [];
 function gtag() { dataLayer.push(arguments); }
