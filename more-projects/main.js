f3ba9023dfae2edf63aacfd7ca193b0ab749c73c
arwildo <arwildo@gmail.com>
2019-11-15 19:07:06 +0800

    add new page More Projects

---
+++ more-projects/main.js
@@ -0,0 +1,23 @@
+// Scroll To Top
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+        document.getElementById("scrollToTop").style.display = "block";
+    } else {
+        document.getElementById("scrollToTop").style.display = "none";
+    }
+   
+}
+
+function topFunction() {
+ 
+     $('html, body').animate({scrollTop:0}, 'slow');
+}
+
+// Google Analytic
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+
+gtag('config', 'UA-51659765-2');
