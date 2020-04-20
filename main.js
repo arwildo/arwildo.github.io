be6880cc057516407042b365cfe08f5984f97377
arwildo <arwildo@gmail.com>
2020-04-20 22:48:46 +0800

    formating

--- main.js
+++ main.js
@@ -8,43 +8,40 @@ closeBtn.addEventListener("click", closeModal);
 window.addEventListener("click", clickOutSide);
 
 function openModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 function clickOutSide(e) {
-    if (e.target == modal) {
-        modal.style.display = "none";
-    }
+  if (e.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 // Scroll To Top
 window.onscroll = function () {
-    scrollFunction();
+  scrollFunction();
 };
 
 function scrollFunction() {
-    if (
-        document.body.scrollTop > 60 ||
-        document.documentElement.scrollTop > 60
-    ) {
-        document.getElementById("scrollToTop").style.display = "block";
-    } else {
-        document.getElementById("scrollToTop").style.display = "none";
-    }
+  if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
+    document.getElementById("scrollToTop").style.display = "block";
+  } else {
+    document.getElementById("scrollToTop").style.display = "none";
+  }
 }
 
 function topFunction() {
-    $("html, body").animate({ scrollTop: 0 }, "slow");
+  $("html, body").animate({ scrollTop: 0 }, "slow");
 }
 
 // Google Analytic
 window.dataLayer = window.dataLayer || [];
 function gtag() {
-    dataLayer.push(arguments);
+  dataLayer.push(arguments);
 }
 gtag("js", new Date());
 
