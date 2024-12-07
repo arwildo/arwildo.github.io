d314c7e6a4fe43f03ba6e9ac0e62e6fa72a65542
Arwildo <arwildo@gmail.com>
2024-12-07 21:30:39 +0900

    feat(layout): add opengraph image preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta property="og:title" content="Arwildo" />
         <meta property="og:description" content="Software Engineer with over 6 years in full-stack development, specializing in creating scalable software solutions and web applications for businesses, hospitals, clinics, and various institutions. Delivering innovative, reliable, and efficient digital solutions tailored to your needs." />
-        <meta property="og:image" content="/images/logo.webp" />
+        <meta property="og:image" content="arwildo-software-web-developer.webp" />
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://arwildo.com/" />
         <script defer src="https://cloud.umami.is/script.js" data-website-id="0c641c91-29c8-40df-8aad-014c850d0743"></script>
